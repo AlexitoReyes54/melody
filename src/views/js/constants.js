54211bb3e8b289863fd7d525e2d93a8b98f6b594
AlexitoReyes54 <alexandereliasr9@gmail.com>
2026-07-03 16:52:30 -0400

    feat: add more loops

--- src/views/js/constants.js
+++ src/views/js/constants.js
@@ -33,5 +33,86 @@ export const loopStore = [
 	{
 		key: 5,
 		loop: ["la", "si", "sol", "mi", "do"],
+	},
+	// --- 20 New Loops Added Below ---
+	{
+		key: 6,
+		loop: ["do", "fa", "la", "do"],
+	},
+	{
+		key: 7,
+		loop: ["re", "fa", "la", "do"],
+	},
+	{
+		key: 8,
+		loop: ["sol", "si", "re", "fa"],
+	},
+	{
+		key: 9,
+		loop: ["mi", "fa", "sol", "la", "si", "do"],
+	},
+	{
+		key: 10,
+		loop: ["do", "re", "mi", "sol", "la"],
+	},
+	{
+		key: 11,
+		loop: ["la", "do", "re", "mi", "sol"],
+	},
+	{
+		key: 12,
+		loop: ["la", "sol", "fa", "mi", "re", "do"],
+	},
+	{
+		key: 13,
+		loop: ["do", "re", "do-sharp", "re", "mi"],
+	},
+	{
+		key: 14,
+		loop: ["la", "la-sharp", "si", "do", "do-sharp"],
+	},
+	{
+		key: 15,
+		loop: ["mi", "sol", "si", "re"],
+	},
+	{
+		key: 16,
+		loop: ["fa", "la", "do", "mi"],
+	},
+	{
+		key: 17,
+		loop: ["do", "sol", "do", "sol"],
+	},
+	{
+		key: 18,
+		loop: ["re", "mi", "fa", "sol", "la"],
+	},
+	{
+		key: 19,
+		loop: ["sol", "fa", "mi", "re", "do"],
+	},
+	{
+		key: 20,
+		loop: ["do", "mi", "re", "fa", "mi", "sol"],
+	},
+	{
+		key: 21,
+		loop: ["si", "la", "sol", "fa"],
+	},
+	{
+		key: 22,
+		loop: ["do-sharp", "fa", "sol-sharp"],
+	},
+	{
+		key: 23,
+		loop: ["re-sharp", "sol", "la-sharp"],
+	},
+	{
+		key: 24,
+		loop: ["do", "re", "mi", "fa", "sol", "la", "si"],
+	},
+	{
+		key: 25,
+		loop: ["si", "sol", "mi", "do"],
 	}
 ];
